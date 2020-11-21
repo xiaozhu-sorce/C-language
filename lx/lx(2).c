fe5cb3b31ea97383bf962c7de86436ec2172f55a
xiaozhu-sorce <790362227@qq.com>
2020-11-22 00:03:16 +0800

    C

--- lx/lx(2).c
+++ lx/lx(2).c
@@ -1,11 +1,12 @@
+//求满足条件xyz+zyx=1231的所有x，y，z;
 #include<stdio.h>
 int main (){
     int x,y,z;
-    for ( x = 0; x < 10; x++)
+    for ( x = 1; x < 10; x++)
     {
         for ( y = 0; y < 10; y++)
         {
-            for ( z = 0; z < 10; z++)
+            for ( z = 1; z < 10; z++)
             {
                 if ((100*x+10*y+z+100*z+10*y+x)==1231)
                 {
