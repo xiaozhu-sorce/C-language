78c42ff3ea7a4a66da5ff56d345e5b516e3e4c80
xiaozhu-sorce <790362227@qq.com>
2021-05-12 23:47:17 +0800

    test

--- C-book/text.c
+++ C-book/text.c
@@ -1,11 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-void main(){
-    int i;
-    int s=1;
-    for ( i = 2; i <= 32; i+=2)
+#include<string.h>
+void main()
+{
+    int i,a;
+    char word[5][10];
+    char max[10], min[10];
+    for (i = 0; i <= 4; i++)
     {
-        s*=i;
+        scanf("%s", word[i]);
     }
-    printf("%d",s);
-    
+
+    strcpy(max, word[0]);
+    strcpy(min, word[0]);
+
+    for (i = 1; i <= 4; i++)
+    {
+        a = strcmp(max, word[i]);
+        if (a < 0)
+        {
+            strcpy(max, word[i]);
+
+        }
+        if (a > 0)
+        {
+            strcpy(min, word[i]);
+
+        }
+    }
+    printf("max:%s min:%s", max, min);
 }
